ac2fc918334379cfbc60336fc925e2b5b3977da3
Philopater Hany <philopaterkiro@gmail.com>
2022-08-07 23:17:24 +0200

    Add files via git

--- Assignments/Assignments From [ 041 ] To [ 046 ]/Assignment 4.py
+++ Assignments/Assignments From [ 041 ] To [ 046 ]/Assignment 4.py
@@ -4,8 +4,6 @@ price = 100
 discount = 30
 
 if country in countries:
-    print(
-        f"Your Country {country} Is Eligible For Discount And The Price After Discount Is ${price - discount}.")
+    print(f"Your Country {country} Is Eligible For Discount And The Price After Discount Is ${price - discount}.")
 else:
-    print(
-        f"Your Country {country} Is Not Eligible For Discount And The Price Is ${price}.")
+    print(f"Your Country {country} Is Not Eligible For Discount And The Price Is ${price}.")
